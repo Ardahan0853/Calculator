ac4837f4cd4edf5524e41bf6195174e33647f57a
Ardahan0853 <ardahan08530853@hotmail.com>
2024-01-29 00:04:48 +0300

    fixed bugs and made a basic design

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const clearBtn = document.querySelector('.clear')
 const result = document.querySelector('.result')
 let numberOne
 let numberTwo
-let operator = ''
+let operator = null
 
 const add = (numberA, numberB) => {
     return numberA + numberB
@@ -30,7 +30,7 @@ const runOperator = (operatorSembol, numberA, numberB) => {
     }else if(operatorSembol == '*'){
         
         return multiply(numberA, numberB)
-    }else{
+    }else if(operatorSembol == '/'){
         
         return divide(numberA, numberB)
     }
@@ -48,7 +48,7 @@ buttons.forEach(button => {
             numberOne = numberArray[0]
             // numberArray.length > 1 ? numberTwo = numberArray[1] : numberTwo = ''
             display.innerText = numberArray[0]
-        }else if(operator.includes(operator)){
+        }else if(operator != null){
             numberArray.push(parseInt(e.target.textContent))
             numberTwo = numberArray[1]
             display.innerText = `${numberArray[0]} ${operator} ${numberArray[1]}`
@@ -60,19 +60,29 @@ buttons.forEach(button => {
         numberOne && numberTwo ? console.log(numberOne, numberTwo) : ''
     })
 })
-clearBtn.addEventListener('click', () => {
+
+const clear = () => {
     numberOne = 0;
     numberTwo = 0;
     numberArray = [];
+    operator = null;
+}
+
+clearBtn.addEventListener('click', () => {
+    clear()
     display.innerText = 'Numbers will be displayed here'
 })
 
 operators.forEach(op => {
     op.addEventListener('click', (e) => {
-
-        operator = e.target.innerText
-        display.innerText += " " + operator
-        console.log(operator)
+        if(numberArray[0]){
+            operator = e.target.innerText   
+            display.innerText = numberOne + " " + operator
+            console.log(operator)
+        }else{
+            alert("Please enter number first")
+        }
+        
     })
 })
 
@@ -80,125 +90,9 @@ operators.forEach(op => {
 result.addEventListener('click', () => {
     if(display.innerText == `${numberArray[0]} ${operator} ${numberArray[1]}`){
         display.innerText = runOperator(operator, numberOne, numberTwo)
+        clear()
     }else{
         alert("Please select numbers and operators first")
     }
     
 })
-
-
-
-
-
-
-
-
-
-
-
-// IT GOT REALLY REALLLLYY MESSY SO I DECICED TO DO IT AGAIN
-// const buttons = document.querySelectorAll('.inside-calc button')
-// const display = document.querySelector('h1')
-// const operators = document.querySelectorAll('.operators')
-
-
-// let numberOne
-
-// let numberTwo
-
-// let operator
-
-
-// var storedValue = []
-
-// var isCleared = false
-
-// let innerTextElement = ''
-
-// buttons.forEach(button => {
-//     button.addEventListener('click', (e) => {
-//         storedValue.push(parseFloat(e.target.innerText))
-        
-//         if(!isCleared){
-//             display.innerText = ''
-//             isCleared = !isCleared
-//             for(let i = 0; i < storedValue.length; i++){
-//                 innerTextElement += storedValue[i]
-//             }
-//             display.innerText = innerTextElement
-//         }else if(storedValue.length > 2){
-//             display.innerText = "Please enter two numbers at a time"
-//             storedValue = []
-//             innerTextElement = ''
-//         }else{
-//             for(let i = storedValue.length - 1; i < storedValue.length; i++){
-//                 innerTextElement += " "+  `${operator ? operator : ''}` +" " + storedValue[i]
-//                 display.innerText = innerTextElement
-//                 console.log(storedValue)
-//             }
-//         }
-//         numberOne = storedValue[0]
-//         numberTwo = storedValue[1]
-//         console.log(parseFloat(e.target.innerText))
-//         console.log(typeof display.innerText)
-//     })
-// })
-
-// operators.forEach(button => {
-//     button.addEventListener('click', (e) => {
-//         operator = e.target.innerText;
-//         innerTextElement +=   ` ${operator ? operator : ''}` + " " 
-//         display.innerText = innerTextElement
-//         console.log(innerTextElement)
-//         if(operator != ''){
-//             operator = ''
-//         }
-        
-//     })
-    
-// })
-
-// document.querySelector('.result').addEventListener('click', () => {
-//     console.log(finalOperator(operator, numberOne, numberTwo))
-//     console.log(operator)   
-    
-// })
-
-
-
-// function finalOperator(op, n1,n2){
-//     if(op === '+'){
-//         add(n1,n2)
-//     }else if(op === '-'){
-//         subtract(n1,n2)
-//     }else if(op === '*'){
-//         multiply(n1, n2)
-//     }else if(op === '/'){
-//         divide(n1,n2)
-//     }
-// }
-
-
-// const add = (n1, n2) => {
-//     let converted1 = parseFloat(n1)
-//     let converted2 = parseFloat(n2)
-//     return converted1 + converted2
-// }
-// const subtract = (n1, n2) => {
-//     let converted1 = parseFloat(n1)
-//     let converted2 = parseFloat(n2)
-//     return converted1 - converted2
-// }
-// const multiply = (n1, n2) => {
-//     let converted1 = parseFloat(n1)
-//     let converted2 = parseFloat(n2)
-//     return converted1 * converted2
-// }
-// const divide = (n1, n2) => {
-//     let converted1 = parseFloat(n1)
-//     let converted2 = parseFloat(n2)
-//     return converted1 / converted2
-// }
-
-
-
